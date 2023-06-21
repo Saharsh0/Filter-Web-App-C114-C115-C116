257ae6997c31915e00876c4a4e99ed2351f57f14
Saharsh0 <113522933+Saharsh0@users.noreply.github.com>
2023-06-21 17:45:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+filterX = 0
+filterY = 0
+
 function preload()
 {
-
+    filter_image = loadImage('https://i.postimg.cc/LXfXLkSM/handlebar-moustache-cartoon-clip-art-cute-mustache-cliparts-removebg-preview.png')
 }
 
 function setup()
@@ -16,7 +19,8 @@ function setup()
 
 function draw()
 {
-
+    image(video, 0, 0, 300, 300);
+    image(filter_image, filterX-50, filterY+7, 100, 30)
 }
 
 function take_snapshot()
@@ -34,6 +38,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        filterX = results[0].pose.nose.x;
+        filterY = results[0].pose.nose.y;                                                                          
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
     }
